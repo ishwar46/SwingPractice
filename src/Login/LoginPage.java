dd7ae20e743f2c0707dbeffb6a5599e290b83c2f
ishwar46 <ishwarura@gmail.com>
2022-05-26 18:04:17 +0545

    Initialize Main Login

---
+++ src/Login/LoginPage.java
@@ -0,0 +1,20 @@
+package Login;
+
+import javax.swing.*;
+import java.awt.*;
+
+class LoginPage  extends JFrame {
+
+ Container cont;
+ JLabel lbl_Title;
+ JLabel lbl_Username;
+ JLabel lbl_Password;
+ JTextField txt_user;
+ JPasswordField password;
+ JRadioButton btn_Male;
+ JRadioButton btn_Female;
+ JButton btn_login;
+ JButton btn_signup;
+
+
+}
